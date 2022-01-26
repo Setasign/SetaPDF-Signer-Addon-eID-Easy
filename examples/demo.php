6a4cf82a128b02f20af653fc8eb41f5e248132a4
Maximilian Kresse <545671+MaximilianKresse@users.noreply.github.com>
2022-01-26 13:40:42 +0100

    Restructured demo

--- examples/demo.php
+++ examples/demo.php
@@ -37,34 +37,9 @@ session_start();
 
 if (isset($_GET['reset'])) {
     unset($_SESSION[__FILE__]);
-}
-
-if (isset($_GET['preview'])) {
-    $doc = file_get_contents($file);
-
-    header('Content-Type: application/pdf');
-    header('Content-Disposition: inline; filename="' . basename($file));
-    header('Expires: 0');
-    header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
-    header('Pragma: public');
-    header('Content-Length: ' . strlen($doc));
-    echo $doc;
-    flush();
-    return;
-}
-
-if (isset($_GET['download'])) {
-    $doc = $_SESSION[__FILE__]['pdf'];
-
-    header('Content-Type: application/pdf');
-    header('Content-Disposition: attachment; filename="' . $doc['name']);
-    header('Expires: 0');
-    header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
-    header('Pragma: public');
-    header('Content-Length: ' . strlen($doc['data']));
-    echo $doc['data'];
-    flush();
-    return;
+    $action = 'start';
+} else {
+    $action = $_GET['action'] ?? 'start';
 }
 
 $module = new Module(
@@ -76,59 +51,48 @@ $module = new Module(
     $settings['sandbox'] ?? false
 );
 
-if (!isset($_SESSION[__FILE__]['processData'])) {
-    $reader = new SetaPDF_Core_Reader_File($file);
-    // let's get the document
-    $document = SetaPDF_Core_Document::load($reader);
+switch ($action) {
+    case 'start':
+        $reader = new SetaPDF_Core_Reader_File($file);
+        // let's get the document
+        $document = SetaPDF_Core_Document::load($reader);
 
-    // now let's create a signer instance
-    $signer = new SetaPDF_Signer($document);
-    $signer->setAllowSignatureContentLengthChange(false);
-    $signer->setSignatureContentLength(36000);
+        // now let's create a signer instance
+        $signer = new SetaPDF_Signer($document);
+        $signer->setAllowSignatureContentLengthChange(false);
+        $signer->setSignatureContentLength(36000);
 
-    // set some signature properties
-    $signer->setLocation($_SERVER['SERVER_NAME']);
-    $signer->setContactInfo('+01 2345 67890123');
-    $signer->setReason('Testing eid easy');
+        // set some signature properties
+        $signer->setLocation($_SERVER['SERVER_NAME']);
+        $signer->setContactInfo('+01 2345 67890123');
+        $signer->setReason('Testing eid easy');
 
-    $field = $signer->getSignatureField();
-    $signer->setSignatureFieldName($field->getQualifiedName());
+        $field = $signer->getSignatureField();
+        $signer->setSignatureFieldName($field->getQualifiedName());
 
-    $tmpDocument = $signer->preSign(new SetaPDF_Core_Writer_File(SetaPDF_Core_Writer_TempFile::createTempPath()), $module);
-    $processData = $module->prepareDocument($tmpDocument, basename($file), $demoUrl);
+        $tmpDocument = $signer->preSign(new SetaPDF_Core_Writer_File(SetaPDF_Core_Writer_TempFile::createTempPath()), $module);
+        $processData = $module->prepareDocument($tmpDocument, basename($file), $demoUrl . '?action=sign');
 
-    $_SESSION[__FILE__] = [
-        'processData' => $processData
-    ];
-} else {
-    $processData = $_SESSION[__FILE__]['processData'];
-    try {
-        // check whether the document is already signed
-        $signatureValue = $module->fetchSignature($processData->getDocId());
-    } catch (\setasign\SetaPDF\Signer\Module\EidEasy\Exception $e) {
-        // todo what kinds of errors can occur?
-        var_dump($e);
-    }
-}
+        $_SESSION[__FILE__] = [
+            'processData' => $processData
+        ];
 
-// if the document isn't signed yet, we need to sign it
-if (!isset($signatureValue)) {
-    // use the web widget here: https://eideasy-widget.docs.eideasy.com/guide/#minimal
-    echo <<<HTML
+        // use the web widget here: https://eideasy-widget.docs.eideasy.com/guide/#minimal
+        echo <<<HTML
 <!DOCTYPE html>
 <head>
     <title>Sign pdf</title>
     <meta charset="utf-8">
 </head>
 <body>
-    <h1>Sign pdf</h1>
     <script src="https://cdn.jsdelivr.net/npm/@eid-easy/eideasy-widget@2.11.1/dist/full/eideasy-widget.umd.min.js"
             integrity="sha256-cDmdyVFN9jvj0dG45ZgSbZ/d8WAhaA4TkmtJRj+ExAQ="
             crossorigin="anonymous"
     ></script>
     <div style="width: 1000px; margin: auto;">
-    <iframe src="{$demoUrl}?preview=1" style="width:100%; height: 500px;"></iframe>
-    <div id="widgetHolder" class="widgetHolder"></div>
+        <h1>Sign pdf</h1>
+        <iframe src="{$demoUrl}?action=preview" style="width:100%; height: 500px;"></iframe>
+        <div id="widgetHolder" class="widgetHolder"></div>
     </div>
     
     <script type="text/javascript">
@@ -141,7 +105,7 @@ const settings = {
   countryCode: 'EE', // ISO 3166  two letter country code
   language: 'en', // ISO 639-1 two letter language code,
   sandbox: true,
-  redirectUri: '{$demoUrl}',
+  redirectUri: '{$demoUrl}?action=sign',
   enabledMethods: {
     signature: 'all'
   },
@@ -175,21 +139,67 @@ widgetHolder.appendChild(eidEasyWidget);
 </script>
 </body>
 HTML;
-    return;
+
+        break;
+
+    case 'preview':
+        $doc = file_get_contents($file);
+
+        header('Content-Type: application/pdf');
+        header('Content-Disposition: inline; filename="' . basename($file));
+        header('Expires: 0');
+        header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+        header('Pragma: public');
+        header('Content-Length: ' . strlen($doc));
+        echo $doc;
+        flush();
+        break;
+
+    case 'sign':
+        if (!isset($_SESSION[__FILE__]['processData'])) {
+            echo 'No process data found.<hr/>If you want to restart the signature process click here: <a href="?reset=1">Restart</a>';
+            break;
+        }
+        $processData = $_SESSION[__FILE__]['processData'];
+        try {
+            // check whether the document is already signed
+            $signatureValue = $module->fetchSignature($processData->getDocId());
+        } catch (\setasign\SetaPDF\Signer\Module\EidEasy\Exception $e) {
+            echo 'Error on signing.';
+            var_dump($e);
+            echo '<hr/>If you want to restart the signature process click here: <a href="?reset=1">Restart</a>';
+            break;
+        }
+
+
+        $reader = new SetaPDF_Core_Reader_File($file);
+        $writer = new SetaPDF_Core_Writer_String();
+
+        $document = SetaPDF_Core_Document::load($reader, $writer);
+        $signer = new SetaPDF_Signer($document);
+        $signer->saveSignature($processData->getTmpDocument(), $signatureValue);
+
+        $_SESSION[__FILE__] = [
+            'pdf' => [
+                'name' => 'signed.pdf',
+                'data' => $writer->getBuffer()
+            ]
+        ];
+
+        echo 'The file was successfully signed. You can download the result <a href="?action=download" download="signed.pdf" target="_blank">here</a>.<hr/>'
+            . ' If you want to restart the signature process click here: <a href="?reset=1">Restart</a>';
+        break;
+
+    case 'download':
+        $doc = $_SESSION[__FILE__]['pdf'];
+
+        header('Content-Type: application/pdf');
+        header('Content-Disposition: attachment; filename="' . $doc['name']);
+        header('Expires: 0');
+        header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+        header('Pragma: public');
+        header('Content-Length: ' . strlen($doc['data']));
+        echo $doc['data'];
+        flush();
+        break;
 }
-
-$reader = new SetaPDF_Core_Reader_File($file);
-$writer = new SetaPDF_Core_Writer_String();
-
-$document = SetaPDF_Core_Document::load($reader, $writer);
-$signer = new SetaPDF_Signer($document);
-$signer->saveSignature($processData->getTmpDocument(), $signatureValue);
-
-$_SESSION[__FILE__] = [
-    'pdf' => [
-        'name' => 'signed.pdf',
-        'data' => $writer->getBuffer()
-    ]
-];
-
-echo 'The file was successfully signed. You can download the result <a href="?download=1" download="signed.pdf" target="_blank">here</a>.';
